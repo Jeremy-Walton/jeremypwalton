20ef5b05627c1400433d8aa9938b0c0244f26b49
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2022-03-30 16:24:02 -0400

    Initial configuration tailwind and postcss

--- app/root.tsx
+++ app/root.tsx
@@ -6,13 +6,29 @@ import {
   Scripts,
   ScrollRestoration,
 } from "remix";
-import type { MetaFunction } from "remix";
+import type { MetaFunction, LinksFunction } from "remix";
+
+import styles from "./styles/tailwind.css";
+import appStyles from './styles/app.css'
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Jeremy P. Walton",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+    { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossOrigin: 'anonymous' },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
+    },
+    { rel: 'stylesheet', href: styles },
+    { rel: 'stylesheet', href: appStyles },
+  ]
+}
 
 export default function App() {
   return (
